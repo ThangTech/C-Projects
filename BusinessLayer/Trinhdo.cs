c8ca1d5316f27190524c0bc4541f0a5170b6bad7
Thang <nguyenvanthangktpm@gmail.com>
2025-04-03 08:27:02 +0700

    C#

---
+++ BusinessLayer/Trinhdo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+namespace BusinessLayer
+{
+    public class Trinhdo
+    {
+        QLNhanSuEntities db = new QLNhanSuEntities();
+        public List<TrinhDo> GetList()
+        {
+            return db.TrinhDoes.ToList();
+        }
+    }
+}
